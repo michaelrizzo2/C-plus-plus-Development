9db000cc9307844c9d40dfcb2e4742c881908d88
Michael Rizzo <m_rizzo@live.com>
2015-04-05 23:42:21 -0400

    about to add logic for setting the course name for the gradebook class

---
+++ fig_04_06_08/fig_04_06_08/GradeBook.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+#include "GradeBook.h"
+
+//Gradebook Constructor
+GradeBook::GradeBook(string name)
+{
+	setCourseName(name);//this will store the coursename
+}
+
+//this will make sure the course name has no more than 25 characters
+void GradeBook::setCourseName(string name)
+{
+
+}
