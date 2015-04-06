b3deea5057bbe3fc16aa593dacc72c5771d0a146
Michael Rizzo <m_rizzo@live.com>
2015-04-05 21:28:55 -0400

    created two gradebook objects in main file

--- fig_03_15_17/fig_03_15_17/fig_03_15_17.cpp
+++ fig_03_15_17/fig_03_15_17/fig_03_15_17.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
-
+#include "GradeBook.h"
 int main()
 
 {
-	
+		//we need to create two gradebook objects
+	Gradebook gradebook1("CS101 introduction to C++");
+	Gradebook gradebook2("C++ Data Structures");
 }
