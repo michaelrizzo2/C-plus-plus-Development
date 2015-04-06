255e85d53ad4006a7638756395be7a0731b965ac
Michael Rizzo <m_rizzo@live.com>
2015-04-05 21:45:20 -0400

    added messages showing the name of both of the gradebook objects

--- fig_03_15_17/fig_03_15_17/fig_03_15_17.cpp
+++ fig_03_15_17/fig_03_15_17/fig_03_15_17.cpp
@@ -7,4 +7,7 @@ int main()
 		//we need to create two gradebook objects
 	Gradebook gradebook1("CS101 introduction to C++");
 	Gradebook gradebook2("C++ Data Structures");
+
+	cout << "the name of the first gradebook is " << gradebook1.getCourseName() << endl;
+	cout << "the name of the second gradebook is " << gradebook2.getCourseName() << endl;
 }
