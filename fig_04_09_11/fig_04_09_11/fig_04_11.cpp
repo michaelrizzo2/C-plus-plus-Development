bee95c06d00195179918d74ac6baaa9ccf795de9
Michael Rizzo <m_rizzo@live.com>
2015-04-07 08:11:31 -0400

    added code for gradebook constructor

---
+++ fig_04_09_11/fig_04_09_11/fig_04_11.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>//used to set precision of output numbers
+using namespace std;
+
+#include"GradeBook.h"
+
+GradeBook::GradeBook(string name)
+{
+	setCourseName(name);
+}
