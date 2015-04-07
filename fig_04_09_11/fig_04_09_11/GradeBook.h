4bf4886b1523df1f0e3f3724aeabf2c25cbdfaaa
Michael Rizzo <m_rizzo@live.com>
2015-04-07 08:03:44 -0400

    Finished Gradebook Header Class file

--- fig_04_09_11/fig_04_09_11/GradeBook.h
+++ fig_04_09_11/fig_04_09_11/GradeBook.h
@@ -4,6 +4,11 @@ using namespace std;
 class GradeBook
 {
 	public:
-
+		GradeBook(string);
+		void setCourseName();
+		string getCourseName();
+		void displayMessage();
+		void determineClassAverage();
 	private:
+		string courseName;
 };
