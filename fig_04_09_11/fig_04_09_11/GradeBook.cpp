5d68c3e821d6ea73ade7877f9316a19fabd60f26
michaelrizzo2 <m_rizzo@live.com>
2015-04-17 21:34:00 -0400

    Constructor for gradebook set up

--- fig_04_09_11/fig_04_09_11/GradeBook.cpp
+++ fig_04_09_11/fig_04_09_11/GradeBook.cpp
@@ -1,2 +1,9 @@
 #include<iostream>
 #include<iomanip>//This will set the accuracy of the output
+//This will include all of the functions in the gradebook header file
+#include "GradeBook.h"
+//This will be for the constructor which will set the string arguement
+GradeBook::GradeBook(string name)
+{
+	setCourseName(name);
+}
