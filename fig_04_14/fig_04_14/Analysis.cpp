7c0cc38be204efc67499f41a50a0f941497f58a6
michaelrizzo2 <m_rizzo@live.com>
2015-04-19 17:14:21 -0400

    Beginning of implementing function that process exam results.Variables initialized for declaration.

---
+++ fig_04_14/fig_04_14/Analysis.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include "Analysis.h"
+
+//This will process the examination results for the 10 students
+void Analysis::processExamResults()
+{
+	//The first step is to initialize the variable declarations
+	int number_of_students_passed = 0;
+	int number_of_students_failed = 0;
+	int studentcounter = 1;
+	int result;
+}
