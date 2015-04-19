f9410493fd62729a12faa95926b70220d3fabdee
michaelrizzo2 <m_rizzo@live.com>
2015-04-19 15:54:05 -0400

    Header file created to examine 10 students grades on an exam.

---
+++ fig_04_14/fig_04_14/Analysis.h
@@ -0,0 +1,8 @@
+//This header file will be for a class that examines the results of an exam
+
+class Analysis
+{
+	public:
+	
+		void processExamResults();//This will process grades for 10 students
+};
