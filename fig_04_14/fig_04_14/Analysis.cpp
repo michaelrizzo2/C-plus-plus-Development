2446f45cf91ac44351da8fe546580b202933f47f
michaelrizzo2 <m_rizzo@live.com>
2015-04-19 21:02:29 -0400

    Fixed error with result variable.

--- fig_04_14/fig_04_14/Analysis.cpp
+++ fig_04_14/fig_04_14/Analysis.cpp
@@ -12,6 +12,9 @@ void Analysis::processExamResults()
 	int result;
 	while (studentcounter<=10)
 	{
+		//This will ask for the grade from the user
+		cout << "Enter 1 for pass or 2 for fail" << endl;
+		cin >> result;
 		// this will check for passes or failures
 		if (result == 1)
 		{
@@ -27,7 +30,7 @@ void Analysis::processExamResults()
 		studentcounter++;
 
 	}
-
+	//This will the num
 	if (number_of_students_passed > 8)
 	{
 		cout << "Raise Tuition" << endl;
