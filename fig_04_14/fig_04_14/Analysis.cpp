128f8bc5c8f135b5ab08c78dd3331efd32c62cc8
michaelrizzo2 <m_rizzo@live.com>
2015-04-19 20:32:41 -0400

    Finished exam grade processor function for analysis class

--- fig_04_14/fig_04_14/Analysis.cpp
+++ fig_04_14/fig_04_14/Analysis.cpp
@@ -10,5 +10,26 @@ void Analysis::processExamResults()
 	int number_of_students_failed = 0;
 	int studentcounter = 0;
 	int result;
+	while (studentcounter<=10)
+	{
+		// this will check for passes or failures
+		if (result == 1)
+		{
+			number_of_students_passed++;
+		}
 
+		else if (result ==2)
+		{
+			number_of_students_failed++;
+		}
+
+		//This will increment the student counter
+		studentcounter++;
+
+	}
+
+	if (number_of_students_passed > 8)
+	{
+		cout << "Raise Tuition" << endl;
+	}
 }
