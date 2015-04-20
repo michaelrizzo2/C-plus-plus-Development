a7ca68d99668af15a00b63cae5ec7ae0622a5f1a
michaelrizzo2 <m_rizzo@live.com>
2015-04-20 16:59:55 -0400

    All public method prototypes for gradebook class written for GradeBook header file.

--- fig_05_09_11/fig_05_09_11/GradeBook.h
+++ fig_05_09_11/fig_05_09_11/GradeBook.h
@@ -1,4 +1,16 @@
 //THis version of gradebook will count the number of A's B's C's D's and F's
 #include<string>
-
+using namespace std;
 //Grade Book class definition
+class GradeBook
+{
+	public:
+		GradeBook(string);//Course constructor
+		void setCourseName(string);
+		string setCourseName();
+		void displayMessage();
+		void input_grades();
+		void displayGradeReport();
+
+
+};
